2a1305d3f92348670d59183e08c9fdd318f6b650
allanzaparoli <allanzaparoli@gmail.com>
2022-04-24 21:40:17 -0300

    item 5 ok

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
 export default function Search() {
+  const [inputSearch, setInputSearch] = useState('');
+  const [buttonDisable, setButtonDisable] = useState(true);
+  const num = 2;
+
+  useEffect(() => {
+    if (inputSearch.length >= num) {
+      setButtonDisable(false);
+    } else {
+      setButtonDisable(true);
+    }
+  }, [inputSearch]);
+
   return (
     <div data-testid="page-search">
       <Header />
+      <form>
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          onChange={ (event) => setInputSearch(event.target.value) }
+          value={ inputSearch }
+        />
+
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ buttonDisable }
+        >
+          Pesquisar
+        </button>
+      </form>
       <p>Search</p>
     </div>
   );
