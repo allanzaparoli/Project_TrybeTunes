05dab0b5336678d5f7a916558564c76343dc3991
allanzaparoli <allanzaparoli@gmail.com>
2022-04-25 09:50:10 -0300

    item 6 ok

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default function Search() {
   const [inputSearch, setInputSearch] = useState('');
   const [buttonDisable, setButtonDisable] = useState(true);
   const num = 2;
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [nomeAlbum, setNomeAlbum] = useState('');
 
   useEffect(() => {
     if (inputSearch.length >= num) {
@@ -14,26 +19,62 @@ export default function Search() {
     }
   }, [inputSearch]);
 
+  async function enviar(event) {
+    event.preventDefault();
+    setLoading(true);
+    setNomeAlbum(inputSearch);
+    setInputSearch('');
+    const resultado = await searchAlbumsAPI(inputSearch);
+    setResult(resultado);
+    setLoading(false);
+  }
+
   return (
     <div data-testid="page-search">
       <Header />
-      <form>
-        <input
-          type="text"
-          data-testid="search-artist-input"
-          onChange={ (event) => setInputSearch(event.target.value) }
-          value={ inputSearch }
-        />
+      { loading ? <h1>Carregando...</h1> : (
+        <form onSubmit={ enviar }>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ (event) => setInputSearch(event.target.value) }
+            value={ inputSearch }
+          />
 
-        <button
-          type="submit"
-          data-testid="search-artist-button"
-          disabled={ buttonDisable }
-        >
-          Pesquisar
-        </button>
-      </form>
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ buttonDisable }
+          >
+            Pesquisar
+          </button>
+        </form>
+      )}
       <p>Search</p>
+      <h2>
+        Resultado de álbuns de:
+        {' '}
+        { nomeAlbum }
+        { result.length === 0 ? <p>Nenhum álbum foi encontrado</p>
+          : result.map((element) => {
+            console.log(element);
+            return (
+              <div key={ element.collectionId }>
+                <Link
+                  data-testid={ `link-to-album-${element.collectionId}` }
+                  to={ `/album/${element.collectionId}` }
+                >
+                  <img
+                    alt="imagem"
+                    key={ element.artistaId }
+                    src={ element.artworkUrl100 }
+                  />
+                  <p>{ element.collectionName }</p>
+                </Link>
+              </div>
+            );
+          })}
+      </h2>
     </div>
   );
 }
