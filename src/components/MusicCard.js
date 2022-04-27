58f75b4ce25c9af65ec7340dea753961987bc333
allanzaparoli <allanzaparoli@gmail.com>
2022-04-27 00:13:19 -0300

    item 8 ok

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
 
 export default function MusicCard(props) {
+  const [loading, setLoading] = useState(false);
+
+  async function favoritar(musica) {
+    setLoading(true);
+    await addSong(musica);
+    setLoading(false);
+  }
   const { getMusica, artist, album } = props;
   return (
     <div>
+      { loading && <h6>Carregando...</h6> }
       <p>MusicCard</p>
       <h3 data-testid="artist-name">
         { artist }
@@ -13,8 +22,17 @@ export default function MusicCard(props) {
       <h3 data-testid="album-name">
         { album }
       </h3>
-      { getMusica.map((element) => (
+      { getMusica.length === 0 ? <h3>Carregando...</h3> : getMusica.map((element) => (
         <>
+          <label htmlFor="Favorita">
+            Favorita
+            <input
+              id="Favorita"
+              type="checkbox"
+              data-testid={ `checkbox-music-${element.trackId}` }
+              onChange={ () => favoritar(element.trackId) }
+            />
+          </label>
           <audio
             data-testid="audio-component"
             key={ element.trackId }
@@ -27,8 +45,6 @@ export default function MusicCard(props) {
               audio
             </code>
           </audio>
-          {/* <h3 data-testid="artist-name">{ element.artistName }</h3> */}
-          <h3>{ element.trackName }</h3>
         </>
       )) }
     </div>
